fa97c779f6fa66694075ddfd2e68dae62dbdfdba
Ansel Adams <ansel@MacMini.local>
2016-02-27 22:59:00 -0700

    add FacebookSDK

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Ansel Adams on 2/27/16.
+//  Copyright © 2016 Ansel Adams. All rights reserved.
+//
+
+// add /Users/ansel/Documents/FacebookSDK to project Build Settings -> Framework Search Paths
+#import <FacebookSDK/FacebookSDK.h>
+//#import </Users/ansel/Documents/FacebookSDK/FacebookSDK.framework/Headers/FacebookSDK.h>
+
